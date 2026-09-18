333b9a1ddbe3d9d1b4e1d8285e3f21156f74aef6
Afrah <afrahaj2007@gmail.com>
2026-09-18 21:31:31 +0530

    Connect authentication to Render backend

--- contractiq_frontend/src/app/services/auth.ts
+++ contractiq_frontend/src/app/services/auth.ts
@@ -1,54 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable, tap } from 'rxjs';
-
-// interface LoginRequest {
-//   email: string;
-//   password: string;
-// }
-
-// interface TokenResponse {
-//   access_token: string;
-//   token_type: string;
-// }
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthService {
-
-//   private readonly baseUrl = 'http://localhost:8080';
-//   private readonly tokenKey = 'contractiq_token';
-
-//   constructor(private http: HttpClient) {}
-
-//   login(email: string, password: string): Observable<TokenResponse> {
-//     const loginData: LoginRequest = {
-//       email,
-//       password
-//     };
-
-//     return this.http
-//       .post<TokenResponse>(`${this.baseUrl}/auth/login`, loginData)
-//       .pipe(
-//         tap(response => {
-//           localStorage.setItem(this.tokenKey, response.access_token);
-//         })
-//       );
-//   }
-
-//   logout(): void {
-//     localStorage.removeItem(this.tokenKey);
-//   }
-
-//   getToken(): string | null {
-//     return localStorage.getItem(this.tokenKey);
-//   }
-
-//   isLoggedIn(): boolean {
-//     return this.getToken() !== null;
-//   }
-// }
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -76,7 +25,7 @@ interface TokenPayload {
 })
 export class AuthService {
 
-  private readonly baseUrl = 'http://localhost:8080';
+  private readonly baseUrl = 'https://contract-obligation-tracking-compliance-zagb.onrender.com';
   private readonly tokenKey = 'contractiq_token';
 
   constructor(private http: HttpClient) {}
